db8bdeeafeadeea13af094b816155c7819d2828f
Mariana Alarcon <alarconmariana438@gmail.com>
2023-11-14 23:12:41 +0100

    hice componentes pregunts

--- src/app/home/medios-pagos/medios-pagos.component.ts
+++ src/app/home/medios-pagos/medios-pagos.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-medios-pagos',
@@ -7,15 +8,4 @@ import { Component } from '@angular/core';
 })
 export class MediosPagosComponent {
   
-//  ngClass
-  activo: string='';
-// ngForLista
-  productos:string[]=['jardin, luces, herramientas']
-  // ngFor tabla
-  carritos:Array<any>=[
-{tipo:'Alambre',marca:'Vigo', precio:2000},
-{tipo:'Mesa',marca:'Vigo', precio:2000},
-{tipo:'silla',marca:'Vigo', precio:4000},  
-
-]
 }
